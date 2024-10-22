89f85eb4b12922de151c742cc9c20e3b2d567e8c
Christian Moser <christianwmoser@proton.me>
2024-10-21 21:49:39 -0600

    add shit

--- src/alpha_service.rs
+++ src/alpha_service.rs
@@ -1,26 +1,29 @@
 use alphavantage::{time_series::TimeSeries, Client};
 use dotenv::dotenv;
 
-struct StockMarket {
+pub struct StockMarket {
     av_client: Client,
+    all_symbols: Vec<String>
 }
 
 impl StockMarket {
-    fn new<T: StockMarketInit>(init: T) -> Self {
-        init.create_stock_market()
-    }
-
-    
-}
-
-pub trait StockMarketInit {
-    fn create_stock_market(self) -> StockMarket;
-}
-
-impl StockMarketInit for Client {
-    fn create_stock_market(self) -> StockMarket {
+    pub fn new() -> Self {
+        dotenv().ok();
         let api_token = std::env::var("AV_KEY");
         let client = Client::new(&api_token.unwrap());
-        StockMarket { av_client: client }
+        StockMarket { av_client: client, all_symbols: vec!["GOOG".to_string()] }
+    }
+
+    pub fn add_symbol(&mut self, new_symbol: String) {
+        self.all_symbols.push(new_symbol);
+    }
+
+    pub async fn get_all_symbols_latest(&self) -> Vec<TimeSeries> {
+        let mut time_series: Vec<TimeSeries> = vec![];
+        for symbol in self.all_symbols.iter() {
+            let temp_time_series = self.av_client.get_time_series_daily_full(symbol).await.unwrap();
+            time_series.push(temp_time_series);
+        }
+        return time_series;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,141 @@
 mod alpha_service;
 
+use std::io;
+
+use alpha_service::{StockMarket};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{layout::Alignment, style::{Style, Stylize}, symbols::{self, border}, text::{Line, Text}, widgets::{block::{Position, Title}, Axis, Block, Chart, Dataset, GraphType, Paragraph, Widget}, DefaultTerminal, Frame};
 use tokio;
 
 #[tokio::main]
-async fn main() {
-    dotenv().ok();
-    let api_token = std::env::var("AV_KEY");
-    let client = Client::new(&api_token.unwrap());
-    let time_series = client.get_time_series_daily("GOOG").await.unwrap();
-    let entry = time_series.entries.last().unwrap();
-    println!("{:?}", entry);
+async fn main() -> io::Result<()> {
 
-    let exchange_rate = client.get_exchange_rate("USD", "EUR").await.unwrap();
-    println!("{:?}", exchange_rate);
+    let mut terminal = ratatui::init();
+
+    let mut stock_market_instance = StockMarket::new();
+
+    let app_result = App {stock_market: stock_market_instance, counter: 0, exit: false}.run(&mut terminal);
+
+    ratatui::restore();
+    app_result
+    // stock_market_instance.add_symbol("NVDA".to_string());
+    // let time_series = stock_market_instance.get_all_symbols_latest().await;
+}
+
+pub struct App {
+    stock_market: StockMarket,
+    counter: u8,
+    exit: bool
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+        where
+            Self: Sized {
+        let title = Title::from(" CLI Street ".bold());
+        let instructions = Title::from(Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+        let block = Block::bordered()
+        .title(title.alignment(Alignment::Center))
+        .title(
+            instructions
+                .alignment(Alignment::Center)
+                .position(Position::Bottom),
+        )
+        .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        let datasets = vec![
+    // Scatter chart
+        Dataset::default()
+            .name("data1")
+            .marker(symbols::Marker::Dot)
+            .graph_type(GraphType::Scatter)
+            .style(Style::default().cyan())
+            .data(&[(0.0, 5.0), (1.0, 6.0), (1.5, 6.434)]),
+        // Line chart
+        Dataset::default()
+            .name("data2")
+            .marker(symbols::Marker::Braille)
+            .graph_type(GraphType::Line)
+            .style(Style::default().magenta())
+            .data(&[(4.0, 5.0), (5.0, 8.0), (7.66, 13.5)]),
+    ];
+
+    // Create the X axis and define its properties
+    let x_axis = Axis::default()
+        .title("X Axis".red())
+        .style(Style::default().white())
+        .bounds([0.0, 10.0])
+        .labels(["0.0", "5.0", "10.0"]);
+
+    // Create the Y axis and define its properties
+    let y_axis = Axis::default()
+        .title("Y Axis".red())
+        .style(Style::default().white())
+        .bounds([0.0, 10.0])
+        .labels(["0.0", "5.0", "10.0"]);
+
+        // Paragraph::new(counter_text).centered().block(block).render(area, buf);
+        Chart::new(datasets)
+            .block(block)
+            .x_axis(x_axis)
+            .y_axis(y_axis)
+            .render(area, buf);
+    }
 }
